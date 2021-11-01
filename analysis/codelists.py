70e3b21e907a4415b32f63f7630ad391108ea09d
Milan Wiedemann <milan.wiedemann@gmail.com>
2021-11-01 16:49:32 +0000

    add blood pressure codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -4,19 +4,38 @@ from config import codelist_path
 # Change the path of the codelist to your chosen codelist
 codelist = codelist_from_csv(codelist_path, system='snomed')
 
-ethnicity_codes = codelist_from_csv(
-        "codelists/opensafely-ethnicity.csv",
-        system="ctv3",
-        column="Code",
-        category_column="Grouping_6",
-    )
-
-nhse_care_homes_codes = codelist_from_csv("codelists/opensafely-nhs-england-care-homes-residential-status.csv",
+bp_dec_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-bpdec_cod.csv",
     system="snomed",
-    column="code",)
-
-ld_codes = codelist_from_csv(
-    "codelists/opensafely-learning-disabilities.csv",
-    system="ctv3",
-    column="CTV3Code",
+    column="code",
 )
+
+bp_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-bp_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+bp_home_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-homebp_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+# COMMENT OUT FOR NOW, DONT NEED THESE CODELISTS AT THE MOMENT
+# ethnicity_codes = codelist_from_csv(
+#         "codelists/opensafely-ethnicity.csv",
+#         system="ctv3",
+#         column="Code",
+#         category_column="Grouping_6",
+#     )
+
+# nhse_care_homes_codes = codelist_from_csv("codelists/opensafely-nhs-england-care-homes-residential-status.csv",
+#     system="snomed",
+#     column="code",)
+
+# ld_codes = codelist_from_csv(
+#     "codelists/opensafely-learning-disabilities.csv",
+#     system="ctv3",
+#     column="CTV3Code",
+# )
