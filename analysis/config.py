e2bc31ce739675c9745aea6ba7e688873228d840
Milan Wiedemann <milan.wiedemann@gmail.com>
2021-11-04 19:20:11 +0000

    add function to do date calculations

--- analysis/config.py
+++ analysis/config.py
@@ -1,17 +1,37 @@
-#study start date.  should match date in project.yaml
+from dateutil.relativedelta import relativedelta
+from datetime import date, datetime
+
+# study start date. should match date in project.yaml
 start_date = "2021-04-01"
 
-#study end date.  should match date in project.yaml
+# study end date. should match date in project.yaml
 end_date = "2022-03-31"
 
-#demographic variables by which code use is broken down
-#select from ["sex", "age_band", "region", "imd", "ethnicity", "learning_disability"]
+# Defining a function for fun, not happy with this but it does the job
+# Defninetely need some help with this python stuff!
+def calculate_date(date, months):
+    """Add or substract months to date.
+    
+    Args:
+        date: date as string in YYYY-MM-DD format.
+        month: Number of month to add (+) or substract (-)
+
+    Returns:
+        Date as string in YYYY-MM-DD format
+    """
+
+    date_calc = datetime.strptime(date, "%Y-%m-%d").date() + relativedelta(months=months)
+    return date_calc.strftime("%Y-%m-%d")
+
+# Define more date variables needed for this project
+end_date_minus_3m = calculate_date(date = end_date, months = -3)
+
+# demographic variables by which code use is broken down
 demographics = ["sex", "age_band", "region", "imd", "ethnicity", "learning_disability", "care_home_status"]
 
 #name of measure
-marker="Blood Pressure"
+marker = "Blood Pressure"
 
 # Codelist path
-# MILAN TODO ADD THE CODELIST I NEED HERE, NOT SURE ABOUT THIS ...
 codelist_path = "codelists/nhsd-primary-care-domain-refsets-bp_cod.csv"
 
