e5438181357050c2b9974e1c404c4d3d24661559
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-04-28 12:30:39 +0300

    add ethnicity study definition

---
+++ analysis/study_definition_ethnicity.py
@@ -0,0 +1,67 @@
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    codelist,
+    codelist_from_csv,
+    Measure,
+)
+
+from config import end_date
+from codelists_demographic import ethnicity6_codes
+
+study = StudyDefinition(
+    default_expectations={
+        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "rate": "uniform",
+    },
+    index_date=end_date,
+    population=patients.satisfying(
+        """
+        NOT has_died
+        AND
+        registered
+        """,
+        has_died=patients.died_from_any_cause(
+            on_or_before="last_day_of_month(index_date)",
+            returning="binary_flag",
+        ),
+        registered=patients.satisfying(
+            "registered_at_start",
+            registered_at_start=patients.registered_as_of("last_day_of_month(index_date)"),
+        ),
+    ),
+    ethnicity=patients.categorised_as(
+        {
+            "Unknown": "DEFAULT",
+            "White": "eth='1'",
+            "Mixed": "eth='2'",
+            "Asian": "eth='3'",
+            "Black": "eth='4'",
+            "Other": "eth='5'",
+        },
+        eth=patients.with_these_clinical_events(
+            ethnicity6_codes,
+            returning="category",
+            find_last_match_in_period=True,
+            include_date_of_match=False,
+            return_expectations={
+                "category": {
+                    "ratios": {"1": 0.2, "2": 0.2, "3": 0.2, "4": 0.2, "5": 0.2}
+                },
+                "incidence": 0.75,
+            },
+        ),
+        return_expectations={
+            "category": {
+                "ratios": {
+                    "White": 0.2,
+                    "Mixed": 0.2,
+                    "Asian": 0.2,
+                    "Black": 0.2,
+                    "Other": 0.2,
+                }
+            },
+            "incidence": 0.8,
+        },
+    ),
+)
