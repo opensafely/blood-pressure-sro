88ba1ad97fcd6af7c0f7d94e948bc2070d9df6ad
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-04-28 12:30:55 +0300

    add config file

---
+++ analysis/config.py
@@ -0,0 +1,9 @@
+# Study start date
+# Note: This should match the start dates in project.yaml
+# In QOF also: Payment Period Start Date (PPSD)
+start_date = "2020-04-01"
+
+# Study end date
+# Note: This should match the end dates in project.yaml
+# In QOF also: Payment Period End Date (PPED)
+end_date = "2021-03-01"
