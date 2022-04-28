bcf3d82e35c7b9904ad911ac386eab6726b7d0d5
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-04-28 16:44:45 +0300

    add bp study def

---
+++ analysis/study_definition_bp002.py
@@ -0,0 +1,59 @@
+from cohortextractor import StudyDefinition, patients, Measure
+
+import json
+import pandas as pd
+
+# Import dates and codelists
+from config import start_date, end_date, demopgraphic_breakdowns
+from codelists_bp import bp_codes, bp_dec_codes
+
+# Import shared variable dictionaries
+from dict_bp_variables import bp002_variables
+from dict_demographic_variables import demographic_variables
+
+study = StudyDefinition(
+    index_date=start_date,
+    default_expectations={
+        "date": {"earliest": start_date, "latest": end_date},
+        "rate": "uniform",
+        "incidence": 0.5,
+    },
+    population=patients.satisfying(
+        """
+        # Define general population parameters
+        (NOT died) AND
+        (sex = 'F' OR sex = 'M') AND
+        (age_band != 'missing') AND
+
+        # Define GMS registration status
+        gms_reg_status AND
+
+        # Define list size type:
+        age >= 45
+        """,
+    ),
+    # Include hypertension and demographic variable dictionaries
+    **demographic_variables,
+    **bp002_variables,
+)
+
+measures = [
+    Measure(
+        id="bp002_population_rate",
+        numerator="bp002_numerator",
+        denominator="population",
+        group_by=["practice"],
+        small_number_suppression=True,
+    ),
+]
+
+# Create hypertension register (hyp_reg) measures
+for breakdown in demopgraphic_breakdowns:
+    m = Measure(
+        id=f"bp002_{breakdown}_breakdown_rate",
+        numerator="bp002_numerator",
+        denominator="bp002_denominator",
+        group_by=[breakdown],
+        small_number_suppression=True,
+    )
+    measures.append(m)
