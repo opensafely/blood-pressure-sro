565e1fb8c94ab686a3694defe99254d4a94c49b0
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-04-28 16:44:17 +0300

    add demographic variables

--- analysis/config.py
+++ analysis/config.py
@@ -1,9 +1,20 @@
 # Study start date
 # Note: This should match the start dates in project.yaml
 # In QOF also: Payment Period Start Date (PPSD)
-start_date = "2020-04-01"
+start_date = "2019-03-01"
 
 # Study end date
 # Note: This should match the end dates in project.yaml
 # In QOF also: Payment Period End Date (PPED)
-end_date = "2021-03-01"
+end_date = "2022-03-01"
+
+# demographic variables by which code use is broken down
+demopgraphic_breakdowns = [
+    "age_band",
+    "sex",
+    "region",
+    "care_home",
+    "learning_disability",
+    "imd",
+    "ethnicity",
+]
